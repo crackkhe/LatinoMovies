f881ec5008ffce723b03cd3d518b9efb04b991c2
crackkhe <157005065+crackkhe@users.noreply.github.com>
2024-01-18 06:44:25 -0500

    dos puntos

--- addon.js
+++ addon.js
@@ -3,7 +3,7 @@ const magnet = require("magnet-uri");
 
 const manifest = { 
     "id": "org.stremio.latinomovies",
-    "version": "2.0.0",
+    "version": "3.0.0",
 
     "name": "Latino Movies",
     "description": "Explora un universo de emocionantes películas en español con nuestro complemento Stremio. Desde éxitos de taquilla hasta joyas cinematográficas ocultas, disfruta de una amplia variedad de géneros. Nuestra colección en constante crecimiento ofrece streaming de alta calidad y opciones para todos los gustos. Descubre el cine en español desde la comodidad de tu pantalla. ¡Instala nuestro addon y sumérgete en un mundo de entretenimiento sin límites! 🎬🍿",
@@ -31,8 +31,8 @@ const manifest = {
 };
 
 const dataset = {
-    "tt5678901": { name: "A Walk To Remember", type: "movie", externalUrl: "https://truelovequest.live/s?a=6420&sm=38105&mt=7&s1=6437965&s2=771670508667409264" },
-    "tt5678901:1:1": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/CrazyTori" },
+    "tt5678901:1:1": { name: "A Walk To Remember", type: "movie", externalUrl: "https://truelovequest.live/s?a=6420&sm=38105&mt=7&s1=6437965&s2=771670508667409264" },
+    "tt5678901:1:2": { name: "A Walk To Remember", type: "movie", externalUrl: "https://es.stripchat.com/CrazyTori" },
     // Agrega más películas con sus respectivos enlaces externos aquí
 };
 
